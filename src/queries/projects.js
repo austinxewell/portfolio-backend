27a2de1deb9ea90c93538ae4b4c1dee41733e1f5
Austin Ewell <austin.ewell86@gmail.com>
2025-11-02 11:29:29 -0700

    :bug: fixed duplicate data responses | added rest client

--- src/queries/projects.js
+++ src/queries/projects.js
@@ -11,25 +11,36 @@ SELECT
     p.live_url,
     p.github_url,
     p.created_at,
-    JSON_ARRAYAGG(
-        JSON_OBJECT(
-        'img_name', i.img_name,
-        'img_url', i.img_url,
-        'is_thumbnail', pi.is_thumbnail
-        )
-    ) AS images,
-    JSON_ARRAYAGG(
-        JSON_OBJECT(
-        'tag_name', tt.tag_name,
-        'is_primary', ptt.is_primary
-        )
-    ) AS tech_tags
+    IFNULL(imgs.images, JSON_ARRAY()) AS images,
+    IFNULL(tags.tech_tags, JSON_ARRAY()) AS tech_tags
 FROM projects p
-LEFT JOIN project_images pi ON pi.project_id = p.id
-LEFT JOIN images i ON i.id = pi.image_id
-LEFT JOIN project_tech_tags ptt ON ptt.project_id = p.id
-LEFT JOIN tech_tags tt ON tt.id = ptt.tag_id
-GROUP BY p.id
+LEFT JOIN (
+    SELECT
+        pi.project_id,
+        JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'img_name', i.img_name,
+                'img_url', i.img_url,
+                'is_thumbnail', pi.is_thumbnail
+            )
+        ) AS images
+    FROM project_images pi
+    JOIN images i ON i.id = pi.image_id
+    GROUP BY pi.project_id
+) imgs ON imgs.project_id = p.id
+LEFT JOIN (
+    SELECT
+        ptt.project_id,
+        JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'tag_name', tt.tag_name,
+                'is_primary', ptt.is_primary
+            )
+        ) AS tech_tags
+    FROM project_tech_tags ptt
+    JOIN tech_tags tt ON tt.id = ptt.tag_id
+    GROUP BY ptt.project_id
+) tags ON tags.project_id = p.id
 ORDER BY p.created_at DESC;
 `;
 
@@ -44,25 +55,36 @@ SELECT
     p.live_url,
     p.github_url,
     p.created_at,
-    JSON_ARRAYAGG(
-        JSON_OBJECT(
-        'img_name', i.img_name,
-        'img_url', i.img_url,
-        'is_thumbnail', pi.is_thumbnail
-        )
-    ) AS images,
-    JSON_ARRAYAGG(
-        JSON_OBJECT(
-        'tag_name', tt.tag_name,
-        'is_primary', ptt.is_primary
-        )
-    ) AS tech_tags
+    IFNULL(imgs.images, JSON_ARRAY()) AS images,
+    IFNULL(tags.tech_tags, JSON_ARRAY()) AS tech_tags
 FROM projects p
-LEFT JOIN project_images pi ON pi.project_id = p.id
-LEFT JOIN images i ON i.id = pi.image_id
-LEFT JOIN project_tech_tags ptt ON ptt.project_id = p.id
-LEFT JOIN tech_tags tt ON tt.id = ptt.tag_id
+LEFT JOIN (
+    SELECT
+        pi.project_id,
+        JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'img_name', i.img_name,
+                'img_url', i.img_url,
+                'is_thumbnail', pi.is_thumbnail
+            )
+        ) AS images
+    FROM project_images pi
+    JOIN images i ON i.id = pi.image_id
+    GROUP BY pi.project_id
+) imgs ON imgs.project_id = p.id
+LEFT JOIN (
+    SELECT
+        ptt.project_id,
+        JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'tag_name', tt.tag_name,
+                'is_primary', ptt.is_primary
+            )
+        ) AS tech_tags
+    FROM project_tech_tags ptt
+    JOIN tech_tags tt ON tt.id = ptt.tag_id
+    GROUP BY ptt.project_id
+) tags ON tags.project_id = p.id
 WHERE p.slug = ?
-GROUP BY p.id
 LIMIT 1;
 `;
