824522e0dea13760a770ae916fb7c2455a7411dc
Austin Ewell <86080954+austinxewell@users.noreply.github.com>
2025-11-05 12:04:36 -0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,21 +1,11 @@
 import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
-  host: process.env.NODE_ENV === 'production' 
-        ? process.env.MYSQLHOST 
-        : process.env.DB_HOST || 'localhost',
-  port: process.env.NODE_ENV === 'production' 
-        ? process.env.MYSQLPORT 
-        : process.env.DB_PORT || 3306,
-  user: process.env.NODE_ENV === 'production' 
-        ? process.env.MYSQLUSER 
-        : process.env.DB_USER || 'root',
-  password: process.env.NODE_ENV === 'production' 
-        ? process.env.MYSQLPASSWORD 
-        : process.env.DB_PASSWORD || '',
-  database: process.env.NODE_ENV === 'production' 
-        ? process.env.MYSQLDATABASE 
-        : process.env.DB_NAME || 'portfolio',
+  host: process.env.MYSQLHOST || process.env.DB_HOST || 'localhost',
+  port: Number(process.env.MYSQLPORT || process.env.DB_PORT || 3306),
+  user: process.env.MYSQLUSER || process.env.DB_USER || 'root',
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || '',
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME || 'portfolio',
 })
 
 export default pool
